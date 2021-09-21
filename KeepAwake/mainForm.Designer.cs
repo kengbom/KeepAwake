a20ea70129845502eaad958ff6d3d2d992e30232
kengbom <57856002+kengbom@users.noreply.github.com>
2021-09-21 13:14:57 +0200

    Add files via upload

---
+++ KeepAwake/mainForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace KeepAwake
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if ( disposing && (components != null) )
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+#region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.Location = new System.Drawing.Point(12, 9);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(222, 38);
+            this.infoLabel.TabIndex = 0;
+            this.infoLabel.Text = "Keeping the system awake...\r\n";
+            this.infoLabel.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 60);
+            this.Controls.Add(this.infoLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = System.Drawing.Icon.ExtractAssociatedIcon(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Keep Awake";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Label infoLabel;
+
+#endregion
+    }
+}
---
+++ KeepAwake/mainForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace KeepAwake
+{
+    // ReSharper disable InconsistentNaming
+    [FlagsAttribute]
+    public enum EXECUTION_STATE : uint
+    {
+        ES_AWAYMODE_REQUIRED = 0x00000040,
+        ES_CONTINUOUS = 0x80000000,
+        ES_DISPLAY_REQUIRED = 0x00000002,
+        ES_SYSTEM_REQUIRED = 0x00000001
+        // ReSharper restore InconsistentNaming
+    }
+    
+    public partial class MainForm : Form
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+        
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        void MainForm_Shown(object sender, EventArgs e)
+        {
+            if ( SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED | 
+                                         EXECUTION_STATE.ES_SYSTEM_REQUIRED |
+                                         EXECUTION_STATE.ES_CONTINUOUS) == 0 )
+            {
+                MessageBox.Show("Error",
+                                "SetThreadExecutionState failed.",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                
+                Environment.Exit(1);
+            }
+        }
+
+        void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+        }
+    }
+}
